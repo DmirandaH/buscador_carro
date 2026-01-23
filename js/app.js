89d7846ef98e255087dafeacc095de65b021e050
mirdayan <dmirandaherrera@gmail.com>
2026-01-22 21:34:57 -0500

    centralizando el texto del html en el css, agregando marca de registro en el html, leyendo el valor de la marca del autómovil.

--- js/app.js
+++ js/app.js
@@ -1,13 +1,35 @@
 
 // Variables
-
-const resultado = document.querySelector('#resultado');
+const marca = document.querySelector('#marca');
 const year = document.querySelector('#year');
+const minimo = document.querySelector('#minimo');
+const maximo = document.querySelector('#maximo');
+const puertas = document.querySelector('#puertas');
+const transmision = document.querySelector('#transmision');
+const color = document.querySelector('#color');
+
+// Contenedor para los resultados
+const resultado = document.querySelector('#resultado');
+
 const max = new Date().getFullYear();
 const min = max - 10;
 
 
 
+// Gnerar un objeto con la busqueda
+const datosBusqueda = {
+    marca: '',
+    year: '',
+    minimo: '',
+    maximo: '',
+    puertas: '',
+    transmision: '',
+    color: ''
+
+  
+
+
+}
 
 
 //Eventos
@@ -23,6 +45,38 @@ document.addEventListener('DOMContentLoaded', () => {
 
 })
 
+// Event listener para los selecct de búsqueda
+marca.addEventListener('change', e => {
+    datosBusqueda.marca = e.target.value;
+
+
+});
+
+year.addEventListener('change', e => {
+    datosBusqueda.year = e.target.value;
+
+    
+});
+
+minimo.addEventListener('change', e => {
+    datosBusqueda.minimo = e.target.value;
+});
+
+maximo.addEventListener('change', e => {
+    datosBusqueda.maximo = e.target.value;
+});
+
+puertas.addEventListener('change', e => {
+    datosBusqueda.puertas = e.target.value;
+});
+
+transmision.addEventListener('change', e => {
+    datosBusqueda.transmision = e.target.value;
+});
+
+color.addEventListener('change', e => {
+    datosBusqueda.color = e.target.value;
+})
 
 
 
