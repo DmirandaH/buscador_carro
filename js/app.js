b43e31a8176ee182d853fed0b128bb01160e594c
mirdayan <dmirandaherrera@gmail.com>
2026-01-29 22:04:34 -0500

    Generando un mensaje de alerta cuando no se obtiene resultados en la búsqeda del filtro

--- js/app.js
+++ js/app.js
@@ -148,10 +148,25 @@ function llenarSelect() {
 function filtrarAuto() {
     const resultado = autos.filter(filtrarMarca).filter(filtrarYear).filter(filtrarMinimo).filter(filtrarMaximo).filter(filtrarPuertas).filter(filtrarTransmision).filter(filtrarColor);
 
-   // console.log(resultado);
 
-   mostrarAutos(resultado);
+
+   if (resultado.length) {
+       mostrarAutos(resultado);
+   } else {
+        noResultado();
+   }
  
+}
+
+function noResultado () {
+
+    limpiarHTML();
+
+    const noResultado = document.createElement('div');
+    noResultado.classList.add('alerta', 'error');
+    noResultado.textContent = 'NO Hay Resultados,intenta una nueva búsqueda con otras opciones';
+    resultado.appendChild(noResultado);
+
 }
 
 function filtrarMarca(auto) {
@@ -226,3 +241,5 @@ function filtrarColor(auto) {
 
     return auto;
 }
+
+
